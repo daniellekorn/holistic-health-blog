0619de6aeaef715b6707fd7cfd0270833a9ab089
Danielle Korn <daniellekorn@zencity.io>
2021-01-16 22:48:18 +0200

    config app js with chakra

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,33 @@
-import '../styles/globals.css'
+import React from 'react';
+import { MDXProvider } from '@mdx-js/react';
+import { DefaultSeo } from 'next-seo';
+import { ChakraProvider } from "@chakra-ui/react"
+import Head from 'next/head';
+import theme from '../styles/theme';
+import MDXComponents from '../components/MDXComponents';
+import SEO from '../next-seo.config';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <MDXProvider components={MDXComponents}>
+          <Head>
+            <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
+            <meta
+              content="width=device-width, initial-scale=1"
+              name="viewport"
+            />
+            <meta content="#ffffff" name="theme-color" />
+            <meta
+              content="/static/favicons/browserconfig.xml"
+              name="msapplication-config"
+            />
+          </Head>
+          <DefaultSeo {...SEO} />
+          <Component {...pageProps} />`
+      </MDXProvider>
+    </ChakraProvider>
+  );
+};
 
-export default MyApp
+export default App;
