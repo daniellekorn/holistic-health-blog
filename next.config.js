8ac163c555f1cc4718b8d2835ed0807c37123873
Danielle Korn <daniellekorn@zencity.io>
2021-01-17 15:01:43 +0200

    image optimization

--- next.config.js
+++ next.config.js
@@ -1,21 +1,32 @@
 const readingTime = require('reading-time');
-const withMdxEnhanced = require('next-mdx-enhanced');
+const withPlugins = require('next-compose-plugins');
+const mdxEnhanced = require('next-mdx-enhanced');
+const optimizedImages = require('next-optimized-images');
 
-module.exports = withMdxEnhanced({
-  layoutPath: 'layouts',
-  defaultLayout: true,
-  remarkPlugins: [
-    require('remark-autolink-headings'),
-    require('remark-slug'),
-  ],
-  extendFrontMatter: {
-    process: (mdxContent) => ({
-      wordCount: mdxContent.split(/\s+/gu).length,
-      readingTime: readingTime(mdxContent)
-    })
-  }
-})({
-  webpack: (config) => {
-    return config;
-  }
-});
+module.exports = withPlugins([
+  [mdxEnhanced, {
+    layoutPath: 'layouts',
+    defaultLayout: true,
+    remarkPlugins: [
+      require('remark-autolink-headings'),
+      require('remark-slug'),
+    ],
+    extendFrontMatter: {
+      process: (mdxContent) => ({
+        wordCount: mdxContent.split(/\s+/gu).length,
+        readingTime: readingTime(mdxContent)
+      })
+    }
+  }],
+  [optimizedImages, {
+    mozjpeg: {
+      quality: 80,
+    },
+    pngquant: {
+      speed: 3,
+      strip: true,
+      verbose: true,
+    },
+    imagesPublicPath: '/holistic-health-blog/_next/static/images/',
+  }],
+]);
